3589b0c16edeb4366a201a3672da61bd9890fc67
Florian Stancioiu <florinvictorstancioiu@gmail.com>
2024-04-02 11:44:47 +0300

    feat: add useState for notified properties

--- cosmoz-treenode-button-view.js
+++ cosmoz-treenode-button-view.js
@@ -1,10 +1,10 @@
-import { html } from 'lit-html';
-import { component } from '@pionjs/pion';
+/* eslint-disable max-statements */
+import { html, nothing } from 'lit-html';
+import { component, useState } from '@pionjs/pion';
 import { css } from './cosmoz-treenode-button-view.styles';
 
 import '@neovici/cosmoz-dialog';
 import { Tree } from '@neovici/cosmoz-tree';
-import { translatable } from '@neovici/cosmoz-i18next';
 
 import './cosmoz-treenode-navigator';
 import { getNode, getTreePathParts } from './helpers';
@@ -55,169 +55,11 @@ const _getButtonLabel = (pathParts, placeholder) => {
 };
 
 /**
- * Event handler for node chip removal button, removes a node chip.
- * @param {object} event Polymer event object.
- * @returns {void}
- */
-const _clearItemSelection = (event) => {
-	const item = event.model.item,
-		selectedIndex = this.selectedNodes.indexOf(item);
-
-	// This will remove from the DOM the source element of the processed event ...
-	this.splice('selectedNodes', selectedIndex, 1);
-	// ... so we must prevent further propagation of this event, because its source is now invalid.
-	// (This has caused troubles in app-drawer-layout click event handler).
-
-	if (!this.selectedNodes.length) {
-		this.reset();
-	}
-
-	event.preventDefault();
-	event.stopPropagation();
-};
-
-/**
- * Whether the reset button should be enabled or not.
- * @param {string} nodePath Node path to check.
- * @param {boolean} noReset Bypass to force disabled.
- * @returns {boolean} Whether the button should be enabled or not.
- */
-const _enableReset = (nodePath, noReset) => {
-	if (noReset) {
-		return false;
-	}
-	return !!nodePath;
-};
-
-/**
- * Get text from a node to set on a node chip.
- * @param {object} node Node to get text from.
- * @returns {string} Chip text.
- */
-const _getChipText = (node) => {
-	return node.name;
-};
-
-/**
- * Open the treenode navigator dialog.
- * @returns {void}
- */
-const openDialogTree = () => {
-	this.$.dialogTree.open();
-};
-
-/**
- * Focus on the treenode navigator in the treenode navigator dialog.
- * @returns {void}
- */
-const focusSearch = () => {
-	this.$.dialogTree.paperDialog
-		.querySelector('#treeNavigator')
-		.shadowRoot.querySelector('cosmoz-input')
-		.focus();
-};
-
-/**
- * Reset the component to make it ready for reuse
- * @returns {void}
- */
-const reset = () => {
-	this.nodePath = '';
-	this.selectedNode = {};
-	this.nodesOnNodePath = [];
-	this.selectedNodes = [];
-	this.highlightedNodePath = '';
-};
-
-/**
- * Select the node in the treenode navigator.
- * @returns {void}
- */
-const selectNode = () => {
-	// nodePath selects the node, without it no selectedNode
-
-	this.selectedNode = getNode(
-		this.highlightedNodePath || this.nodePath,
-		this.tree,
-	);
-	if (this.highlightedNodePath) {
-		this.nodePath = this.highlightedNodePath;
-	}
-	this.nodesOnNodePath = getTreePathParts(
-		this.highlightedNodePath || this.nodePath,
-		this.tree,
-	);
-
-	if (this.multiSelection) {
-		if (
-			!this.selectedNodes.some(
-				(node) => node.pathLocator === this.highlightedNodePath,
-			)
-		) {
-			this.push('selectedNodes', this.selectedNode);
-		}
-		this.nodePath = '';
-		this.selectedNode = {};
-	}
-	this.$.dialogTree.close();
-};
-
-/**
- * Selects node and closes the dialog
- * @returns {void}
- */
-const _selectNodeAndCloseDialog = () => {
-	this.selectNode();
-};
-
-/**
- * Determine if selected nodes container should be visible or not.
- * @param {boolean} multiSelection Multi selection setting.
- * @param {number} selectedNodesLength Selected nodes quantity.
- * @returns {boolean} Whether the selected nodes container should be visible or not.
- */
-const _showSelectedNodes = (multiSelection, selectedNodesLength) => {
-	return multiSelection && selectedNodesLength > 0;
-};
-
-/**
- * Callback event handler to refit the treenode navigator dialog when
- * data plane has changed.
- * @returns {void}
- */
-const refit = () => {
-	this._debouncer = Debouncer.debounce(
-		this._debouncer,
-		timeOut.after(50), // 5 was enough during test
-		() => {
-			this.$.dialogTree.fit();
-		},
-	);
-};
-
-const onOpened = () => {
-	this.opened = true;
-	this.focusSearch();
-};
-
-const onClosed = () => {
-	this.opened = false;
-};
-
-/**
  * Navigator through object with treelike datastructure.
  */
 export const CosmozTreenodeButtonView = ({
 	multiSelection = false,
 	tree,
-	/*
-	 * Currently selected node object
-	 */
-	selectedNode = {},
-	/**
-	 * Selected nodes
-	 */
-	selectedNodes = [],
 	/**
 	 * If true, reset button gets hidden
 	 */
@@ -235,15 +77,6 @@ export const CosmozTreenodeButtonView = ({
 	 */
 	buttonTextPlaceholder = getButtonTextPlaceholder(),
 	buttonText = _getButtonLabel(nodesOnNodePath, buttonTextPlaceholder),
-
-	/*
-	 * The path of the selected node
-	 */
-	nodePath,
-	/*
-	 * The nodes on the path of the selected node
-	 */
-	nodesOnNodePath,
 	/*
 	 * Text displayed when local search has finished
 	 * to suggest a search on the entire tree
@@ -258,11 +91,178 @@ export const CosmozTreenodeButtonView = ({
 	 * starts.
 	 */
 	searchMinLength = 1,
+}) => {
+	/*
+	 * The path of the selected node
+	 */
+	const [nodePath, setNodePath] = useState('');
+	/*
+	 * Currently selected node object
+	 */
+	const [selectedNode, setSelectedNode] = useState({});
+	/*
+	 * The nodes on the path of the selected node
+	 */
+	const [nodesOnNodePath, setNodesOnNodePath] = useState([]);
+	/**
+	 * Selected nodes
+	 */
+	const [selectedNodes, setSelectedNodes] = useState([]);
 	/*
 	 * Path string of highlighted (focused) node
 	 */
-	highlightedNodePath,
-}) => {
+	const [highlightedNodePath, setHighlightedNodePath] = useState('');
+
+	/**
+	 * Event handler for node chip removal button, removes a node chip.
+	 * @param {object} event Polymer event object.
+	 * @returns {void}
+	 */
+	const _clearItemSelection = (event) => {
+		const item = event.model.item,
+			selectedIndex = this.selectedNodes.indexOf(item);
+
+		// This will remove from the DOM the source element of the processed event ...
+		this.splice('selectedNodes', selectedIndex, 1);
+		// ... so we must prevent further propagation of this event, because its source is now invalid.
+		// (This has caused troubles in app-drawer-layout click event handler).
+
+		if (!this.selectedNodes.length) {
+			this.reset();
+		}
+
+		event.preventDefault();
+		event.stopPropagation();
+	};
+
+	/**
+	 * Whether the reset button should be enabled or not.
+	 * @param {string} nodePath Node path to check.
+	 * @param {boolean} noReset Bypass to force disabled.
+	 * @returns {boolean} Whether the button should be enabled or not.
+	 */
+	const _enableReset = (nodePath, noReset) => {
+		if (noReset) {
+			return false;
+		}
+		return !!nodePath;
+	};
+
+	/**
+	 * Get text from a node to set on a node chip.
+	 * @param {object} node Node to get text from.
+	 * @returns {string} Chip text.
+	 */
+	const _getChipText = (node) => {
+		return node.name;
+	};
+
+	/**
+	 * Open the treenode navigator dialog.
+	 * @returns {void}
+	 */
+	const openDialogTree = () => {
+		this.$.dialogTree.open();
+	};
+
+	/**
+	 * Focus on the treenode navigator in the treenode navigator dialog.
+	 * @returns {void}
+	 */
+	const focusSearch = () => {
+		this.$.dialogTree.paperDialog
+			.querySelector('#treeNavigator')
+			.shadowRoot.querySelector('cosmoz-input')
+			.focus();
+	};
+
+	/**
+	 * Reset the component to make it ready for reuse
+	 * @returns {void}
+	 */
+	const reset = () => {
+		setNodePath('');
+		setSelectedNode({});
+		setNodesOnNodePath([]);
+		setSelectedNodes([]);
+		setHighlightedNodePath('');
+	};
+
+	/**
+	 * Select the node in the treenode navigator.
+	 * @returns {void}
+	 */
+	const selectNode = () => {
+		// nodePath selects the node, without it no selectedNode
+
+		this.selectedNode = getNode(
+			this.highlightedNodePath || this.nodePath,
+			this.tree,
+		);
+		if (this.highlightedNodePath) {
+			this.nodePath = this.highlightedNodePath;
+		}
+		this.nodesOnNodePath = getTreePathParts(
+			this.highlightedNodePath || this.nodePath,
+			this.tree,
+		);
+
+		if (this.multiSelection) {
+			if (
+				!this.selectedNodes.some(
+					(node) => node.pathLocator === this.highlightedNodePath,
+				)
+			) {
+				this.push('selectedNodes', this.selectedNode);
+			}
+			this.nodePath = '';
+			this.selectedNode = {};
+		}
+		this.$.dialogTree.close();
+	};
+
+	/**
+	 * Selects node and closes the dialog
+	 * @returns {void}
+	 */
+	const _selectNodeAndCloseDialog = () => {
+		this.selectNode();
+	};
+
+	/**
+	 * Determine if selected nodes container should be visible or not.
+	 * @param {boolean} multiSelection Multi selection setting.
+	 * @param {number} selectedNodesLength Selected nodes quantity.
+	 * @returns {boolean} Whether the selected nodes container should be visible or not.
+	 */
+	const _showSelectedNodes = (multiSelection, selectedNodesLength) => {
+		return multiSelection && selectedNodesLength > 0;
+	};
+
+	/**
+	 * Callback event handler to refit the treenode navigator dialog when
+	 * data plane has changed.
+	 * @returns {void}
+	 */
+	const refit = () => {
+		this._debouncer = Debouncer.debounce(
+			this._debouncer,
+			timeOut.after(50), // 5 was enough during test
+			() => {
+				this.$.dialogTree.fit();
+			},
+		);
+	};
+
+	const onOpened = () => {
+		this.opened = true;
+		this.focusSearch();
+	};
+
+	const onClosed = () => {
+		this.opened = false;
+	};
+
 	return html`
 		${css}
 		<div class="actions">
@@ -295,7 +295,7 @@ export const CosmozTreenodeButtonView = ({
 							</div>`,
 					)}
 				</div>`
-			: html``}
+			: nothing}
 
 		<cosmoz-dialog
 			id="dialogTree"
