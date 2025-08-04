2d7b223ce6092137151caad11ca26ce17f20d1c5
basilgood <basilgood@users.noreply.github.com>
2025-08-04 08:28:39 +0000

    Deploy preview for PR 163 🛫

---
+++ pr/pr-163/assets/cosmoz-treenode-button-view.stories-CfzPd_d0.js
@@ -0,0 +1,66 @@
+import{x as b}from"./lit-element-D5YJkDF8.js";import{T as p,a as T}from"./tree-data-DDf9mH4V.js";import"./cosmoz-treenode-button-view-BmKvfF5S.js";import"./iframe-aInBdM9S.js";const v={title:"Components/CosmozTreenodeButtonView",component:"cosmoz-treenode-button-view",tags:["autodocs"],argTypes:{buttonTextPlaceholder:{control:"text"},dialogText:{control:"text"},searchPlaceholder:{control:"text"},searchGlobalPlaceholder:{control:"text"},searchMinLength:{control:"number"},searchDebounceTimeout:{control:"number"},noReset:{control:"boolean"},multiSelection:{control:"boolean"}}},a=e=>{const m=new p(T);return b`
+        <div style="padding: 20px;">
+            <cosmoz-treenode-button-view
+                .tree=${m}
+                button-text-placeholder=${e.buttonTextPlaceholder}
+                dialog-text=${e.dialogText}
+                search-placeholder=${e.searchPlaceholder}
+                search-global-placeholder=${e.searchGlobalPlaceholder}
+                .searchMinLength=${e.searchMinLength}
+                .searchDebounceTimeout=${e.searchDebounceTimeout}
+                .noReset=${e.noReset}
+                .multiSelection=${e.multiSelection}
+            ></cosmoz-treenode-button-view>
+        </div>
+    `},t=a.bind({});t.args={buttonTextPlaceholder:"Select a node",dialogText:"Search or navigate to chosen destination",searchPlaceholder:"Search...",searchGlobalPlaceholder:"Click to search again but globally",searchMinLength:3,searchDebounceTimeout:2e3,noReset:!1,multiSelection:!1};const o=a.bind({});o.args={buttonTextPlaceholder:"Select multiple nodes",dialogText:"Search or navigate to chosen destination",searchPlaceholder:"Search...",searchGlobalPlaceholder:"Click to search again but globally",searchMinLength:3,searchDebounceTimeout:2e3,noReset:!1,multiSelection:!0};const r=a.bind({});r.args={buttonTextPlaceholder:"Select a node (no reset)",dialogText:"Search or navigate to chosen destination",searchPlaceholder:"Search...",searchGlobalPlaceholder:"Click to search again but globally",searchMinLength:3,searchDebounceTimeout:2e3,noReset:!0,multiSelection:!1};var n,l,c;t.parameters={...t.parameters,docs:{...(n=t.parameters)==null?void 0:n.docs,source:{originalSource:`args => {
+  const tree = new DefaultTree(adminFilesTree);
+  return html\`
+        <div style="padding: 20px;">
+            <cosmoz-treenode-button-view
+                .tree=\${tree}
+                button-text-placeholder=\${args.buttonTextPlaceholder}
+                dialog-text=\${args.dialogText}
+                search-placeholder=\${args.searchPlaceholder}
+                search-global-placeholder=\${args.searchGlobalPlaceholder}
+                .searchMinLength=\${args.searchMinLength}
+                .searchDebounceTimeout=\${args.searchDebounceTimeout}
+                .noReset=\${args.noReset}
+                .multiSelection=\${args.multiSelection}
+            ></cosmoz-treenode-button-view>
+        </div>
+    \`;
+}`,...(c=(l=t.parameters)==null?void 0:l.docs)==null?void 0:c.source}}};var s,i,h;o.parameters={...o.parameters,docs:{...(s=o.parameters)==null?void 0:s.docs,source:{originalSource:`args => {
+  const tree = new DefaultTree(adminFilesTree);
+  return html\`
+        <div style="padding: 20px;">
+            <cosmoz-treenode-button-view
+                .tree=\${tree}
+                button-text-placeholder=\${args.buttonTextPlaceholder}
+                dialog-text=\${args.dialogText}
+                search-placeholder=\${args.searchPlaceholder}
+                search-global-placeholder=\${args.searchGlobalPlaceholder}
+                .searchMinLength=\${args.searchMinLength}
+                .searchDebounceTimeout=\${args.searchDebounceTimeout}
+                .noReset=\${args.noReset}
+                .multiSelection=\${args.multiSelection}
+            ></cosmoz-treenode-button-view>
+        </div>
+    \`;
+}`,...(h=(i=o.parameters)==null?void 0:i.docs)==null?void 0:h.source}}};var d,u,g;r.parameters={...r.parameters,docs:{...(d=r.parameters)==null?void 0:d.docs,source:{originalSource:`args => {
+  const tree = new DefaultTree(adminFilesTree);
+  return html\`
+        <div style="padding: 20px;">
+            <cosmoz-treenode-button-view
+                .tree=\${tree}
+                button-text-placeholder=\${args.buttonTextPlaceholder}
+                dialog-text=\${args.dialogText}
+                search-placeholder=\${args.searchPlaceholder}
+                search-global-placeholder=\${args.searchGlobalPlaceholder}
+                .searchMinLength=\${args.searchMinLength}
+                .searchDebounceTimeout=\${args.searchDebounceTimeout}
+                .noReset=\${args.noReset}
+                .multiSelection=\${args.multiSelection}
+            ></cosmoz-treenode-button-view>
+        </div>
+    \`;
+}`,...(g=(u=r.parameters)==null?void 0:u.docs)==null?void 0:g.source}}};const D=["Default","WithMultiSelection","WithNoReset"];export{t as Default,o as WithMultiSelection,r as WithNoReset,D as __namedExportsOrder,v as default};
