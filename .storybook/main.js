01d07ea6a15a7ca1dea33c074d6e342e4dfa6b1e
basilgood <elsile69@yahoo.com>
2025-09-10 03:33:33 +0300

    fix: add storybook plugin for documentation

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,13 @@
 export default {
-	stories: ['../stories/**/*.stories.@(js|jsx|ts|tsx)'],
+	stories: [
+		'../stories/**/*.mdx',
+		'../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)',
+	],
 	framework: {
 		name: '@storybook/web-components-vite',
 		options: {},
 	},
+	addons: ['@storybook/addon-docs'],
 	docs: {
 		autodocs: 'tag',
 	},
